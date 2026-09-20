090f2d6f6c4d5362d24e4f7a1a046f7b759f4557
evelyn-keefner <evelyn0keefner@gmail.com>
2026-09-20 15:10:08 -0400

    more testing stages, removed debug message

--- etc/generate_init_files.py
+++ etc/generate_init_files.py
@@ -1,17 +1,24 @@
-import system
+import os
+INIT_FILES = ["initStages.msc", "initEvilStages.msc"]
 
-INITSTAGESFILENAME = "initStages.msc"
+def openFileToReadErrorChecked(filename:str):
+    try:
+        f = open(filename, "r")
+    except:
+        print(f"{filename} failed to open. Exiting...")
+        exit(1)
+    return f
 
-def openFileToWriteErrorChecked(filename:str) -> TextIOWrapper:
-    try: 
+def openFileToWriteErrorChecked(filename:str):
+    try:
         f = open(filename, "w")
     except:
         print(f"{filename} failed to open. Exiting...")
         exit(1)
     return f
 
-def openFileToAppendErrorChecked(filename:str) -> TextIOWrapper:
-    try: 
+def openFileToAppendErrorChecked(filename:str):
+    try:
         f = open(filename, "a")
     except:
         print(f"{filename} failed to open. Exiting...")
@@ -26,11 +33,11 @@ def initialPrompt() -> int:
     print("\t2. Delete an init file")
 
     while (awaiting_input):
-        choice = input("Enter selection: (as a number)")
+        choice = input("Enter selection: (as a number)\n")
         try:
             choice = int(choice)
 
-            if (choice != 1 && choice != 2):
+            if (choice != 1 and choice != 2):
                 raise Exception()
 
             awaiting_input = False
@@ -39,14 +46,71 @@ def initialPrompt() -> int:
 
     return choice
 
+def appendStackToFilePrompt() -> int:
+    awaiting_input = True;
+    os.system('cls||clear')
+    print("[nbparty]")
+    print("\t1. Append a stack of stages to init file")
+    print("\t2. Delete an init file")
+
+    while (awaiting_input):
+        choice = input("Enter selection: (as a number)\n")
+        try:
+            choice = int(choice)
+
+            if (choice != 1 and choice != 2):
+                raise Exception()
+
+            awaiting_input = False
+        except:
+            print("Incorrect input. Please enter another number.")
+
+    return choice
+
+def deleteInitFilePrompt() -> int:
+    awaiting_input = True;
+    os.system('cls||clear')
+    print("[nbparty]")
+    for i, init_file in enumerate(INIT_FILES):
+        print(f"{i}: {init_file}")
+
+    while (awaiting_input):
+        choice = input("Enter selection: (as a number)\n")
+        try:
+            choice = int(choice)
+
+            if (choice != 1 and choice != 2):
+                raise Exception()
+
+            awaiting_input = False
+        except:
+            print("Incorrect input. Please enter another number.")
+
+    return choice
 
 def main():
     f = openFileToWriteErrorChecked(INITSTAGESFILENAME)
 
     match initialPrompt():
         case 1:
-            
+            match appendStackToFilePrompt():
+                case 1:
+                    pass
+                case 2:
+                    pass
+                case _:
+                    pass
         case 2:
+            match deleteInitFilePrompt():
+                case 1:
+                    pass
+                case 2:
+                    pass
+                case _:
+                    pass
+        case _:
+            print("initialPrompt returned a bad value. Exiting...")
+            exit(1)
     # TODO 
     # - 1. Append to init file from stage stack
     #       * prompts user for which file
